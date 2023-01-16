5a5b662bd7608652f127a4b3de5cef153ec5c563
krisbitney <kristoferbitney@hotmail.com>
2023-01-16 20:38:10 +0530

    refactored invokeWithTimeout

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  "roots": [
-    "<rootDir>"
-  ],
-  "testMatch": [
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+  roots: ["<rootDir>"],
+  testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest", {
+        tsconfig: "tsconfig.json",
+        diagnostics: false
+      }]
   },
 }
 
