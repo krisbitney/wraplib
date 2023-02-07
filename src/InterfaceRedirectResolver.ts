0d8635c0d159424c1e013f8b8908686db474769f
krisbitney <kristoferbitney@hotmail.com>
2023-02-07 17:55:08 +0530

    added draft implementation of InterfaceRedirectResolver (untested)

---
+++ src/InterfaceRedirectResolver.ts
@@ -0,0 +1,36 @@
+import { ResolverWithHistory, UriResolutionResult } from "@polywrap/uri-resolvers-js";
+import {
+  CoreClient,
+  IUriResolutionContext,
+  Result,
+  Uri,
+  UriPackageOrWrapper,
+} from "@polywrap/core-js";
+
+export class InterfaceRedirectResolver extends ResolverWithHistory {
+  private redirects: Map<string, Uri> = new Map();
+  private initialized: boolean;
+
+  protected getStepDescription = (uri: Uri): string =>
+    `InterfaceRedirect (${uri.uri} - ${this.redirects.get(uri.uri)})`;
+
+  private setInterfaceRedirects(client: CoreClient): void {
+    client.getInterfaces()?.forEach((i) => i.implementations.length && this.redirects.set(i.interface.uri, i.implementations[0]));
+  }
+
+  protected async _tryResolveUri(
+    uri: Uri,
+    client: CoreClient,
+    resolutionContext: IUriResolutionContext
+  ): Promise<Result<UriPackageOrWrapper>> {
+    if (!this.initialized) {
+      this.setInterfaceRedirects(client);
+      this.initialized = true;
+    }
+    const implementation = this.redirects.get(uri.uri);
+    if (implementation) {
+      return UriResolutionResult.ok(implementation);
+    }
+    return UriResolutionResult.ok(uri);
+  }
+}
